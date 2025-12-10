e1900cec23286207bf48fb1037bbf10eae8ecb70
Sara Rodríguez Pardo <rodriguezps@easdalcoi.es>
2025-12-10 17:29:42 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let myFont;
+let cp=document.getElementById("colorpicker");
+
+function preload(){
+  myFont=loadFont("../fonts/Amakan-Strips.otf")
+
+}
+function setup() {
+  var c=createCanvas(400, 400);
+  c.parent("canvasWrapper")
+}
+
+function draw() {
+  background(cp.value);
+  textAlign(CENTER);
+  textSize(25);
+  textFont(myFont);
+  fill(255);
+  text("feliz navidad", width/2, height/2);
+}
